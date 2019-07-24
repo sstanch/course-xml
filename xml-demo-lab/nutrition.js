45abd3da0777f7daa955c79d94313a1136296522
Trayan Iliev <office@iproduct.org>
2019-07-24 17:28:22 +0300

    csv to xml demo completed

--- xml-demo-lab/nutrition.js
+++ xml-demo-lab/nutrition.js
@@ -19,6 +19,19 @@ function init() {
     const root = xml.createElement('nutrition');
     xml.appendChild(root);
 
+    const lines = xhr.responseText.split(/[\n\r]+/);
+    console.log(lines);
+    for(let i = 2; i < lines.length - 1; i++) {
+      const foodElem = xml.createElement('food');
+      root.appendChild(foodElem);
+      const fields = lines[i].split(';');
+      for(let j = 0; j < fields.length; j++) {
+        const elem = xml.createElement(FOOD_CHILD[j]);
+        const text = xml.createTextNode(fields[j]);
+        elem.appendChild(text);
+        foodElem.appendChild(elem);
+      }
+    }
     const serializer = new XMLSerializer();
     document.getElementById('xml').value = serializer.serializeToString(xml);
   };
