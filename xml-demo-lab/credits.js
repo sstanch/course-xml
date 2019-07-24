d1684526cfe5dfca6ed91fb9d3abab745bc3f2d2
Trayan Iliev <office@iproduct.org>
2019-07-24 15:51:33 +0300

    history visualized

--- xml-demo-lab/credits.js
+++ xml-demo-lab/credits.js
@@ -10,6 +10,22 @@ function init() {
                 return;
         }
         document.getElementById('xml-document').value = xhr.responseText;
+        xmlDoc = xhr.responseXML;
+        prevCredits = xmlDoc.getElementsByTagName('PreviousCredit');
+        let result = '<table><tr><th>Credit</th><th>Begin Date</th><th>Complete Date</th><th>Create Date</th>'
+         + '<th>Credit Max Delay</th><th>creditPrincipalAmount</th>';
+        for(let i = 0; i < prevCredits.length; i++) {
+            let creditNode = prevCredits[i];
+            result += `<tr><td>Previous credit ${i + 1}</td>`;
+            const attrList = creditNode.attributes;
+            for(let j = 0; j < Math.min(attrList.length, 5); j++){
+                const attr = attrList.item(j);
+                result += `<td>${attr.value}</td>`;
+            }
+            result += '</tr>';
+        }
+        result += '</table>';
+        document.getElementById('previous-credits').innerHTML = result;
     }
     xhr.send();
 }
